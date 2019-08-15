dc9f77e96f23833f145fe0fec1ac0dcb2fd97187
Ayushmaan001 <54117681+Ayushmaan001@users.noreply.github.com>
2019-08-15 13:12:45 +0530

    Add intial cv website files

---
+++ site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+ <?php // include 'files.html'; ?>
+<!--
+<form action="site.php" method="post">
+number:<input type="number" name="num1">
+<input type = "submit">
+</form> -->
+<?php
+
+
+
+
+?>
+  </body>
+</html>
+<!-- comment syntax -->
